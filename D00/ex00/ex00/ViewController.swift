bfae0e16bd8dc71fdcd147663fd70ba8d2435ce2
stephane <phaninho@live.fr>
2018-05-25 09:10:47 +0200

    add ex00

---
+++ D00/ex00/ex00/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  ex00
+//
+//  Created by Stephane MARTINS on 5/25/18.
+//  Copyright © 2018 Stephane MARTINS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func helloBtn(_ sender: UIButton) {
+        print("Hello world !");
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
