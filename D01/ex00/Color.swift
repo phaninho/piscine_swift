dce381351175f273a18a8fcbfa312a25b99a3663
phaninho <phaninho@live.fr>
2018-05-27 01:54:00 +0200

    add ex00 all done

---
+++ D01/ex00/Color.swift
@@ -0,0 +1,8 @@
+
+enum    Color: String{
+    case Eau = "bleu"
+    case Feu = "rouge"
+    case Air = "blanc"
+    case Terre = "noir"
+    static let allColors: [Color] = [Eau, Feu, Air, Terre]
+}
