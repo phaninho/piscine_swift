c7697aa48f121ef8c5891f924fcda186aa44e696
phaninho <phaninho@live.fr>
2018-05-27 02:07:16 +0200

    add a main test

---
+++ D01/ex00/main.swift
@@ -0,0 +1,12 @@
+
+print("=== Toutes les couleurs ===\n")
+for col in Color.allColors
+{
+    print("\(col) -> \(col.rawValue)")
+}
+print("\n\n=== Toutes les valeurs ===\n")
+
+for val in Value.allValues
+{
+    print("\(val) -> \(val.rawValue)")
+}
