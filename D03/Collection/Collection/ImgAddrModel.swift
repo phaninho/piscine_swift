1e33ed6a8635654d53937332f4574647991da86e
phaninho <phaninho@live.fr>
2018-05-31 22:08:22 +0200

    add first ex

--- D03/Collection/Collection/ImgAddrModel.swift
+++ D03/Collection/Collection/ImgAddrModel.swift
@@ -6,5 +6,6 @@ struct DataAddr {
         ("https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/pluto_color_mapmosaic.jpg"),
         ("https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/pia22238.jpg"),
         ("https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/pia22326-16.jpg")
+        
     ]
 }
